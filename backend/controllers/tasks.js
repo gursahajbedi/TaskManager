64e786774776c36b2f47bf2a3fabe6a90890a6d4
Gursahaj Singh <gursahajsinghkhalsa@gmail.com>
2023-04-03 22:33:15 +0530

    new commit

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -28,7 +28,7 @@ const createTask =async (req,res)=>{
 const updateTask = async(req,res)=>{
     const id=req.params.id
     try{
-        const update= await Tasks.findOneAndUpdate({id:id},req.body)
+        const update= await Tasks.findOneAndUpdate({_id:id},req.body)
         res.status(200).json({updated:update.id})
     }
     catch(err){
